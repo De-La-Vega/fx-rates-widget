7dd3d351ba5c060e5222e0eaed285fd00f917a79
Vitaliy Kirenkov <vkirenk2@ru.ibm.com>
2017-07-08 00:33:06 +0300

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# fx-rates-widget
-Widget with fetching rates
+# Currency rates widget
+
+### To run
+
+* You'll need to have [git](https://git-scm.com/) and [node](https://nodejs.org/en/) installed in your system.
+* Fork and clone the project:
+
+```
+git clone https://github.com/De-La-Vega/fx-rates-widget.git
+```
+
+* Then install the dependencies:
+
+```
+npm install
+```
+
+* Run development server:
+
+```
+npm start
+```
+
+* Open in browser:
+
+```
+http://localhost:8888/
+```
---
+++ src/app.jsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import axios from 'axios';
+import '../styles/index.scss';
+
+export default class App extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            rates: []
+        }
+    }
+
+    componentDidMount () {
+        this.startPolling();
+    }
+
+    componentWillUnmount () {
+        if (this.pollingTimer) {
+            clearInterval(this.pollingTimer);
+            this.pollingTimer = null;
+        }
+    }
+
+    /**
+     * Starting polling.
+     */
+    startPolling () {
+        this.fetchData();
+
+        this.pollingTimer = setInterval(this.fetchData, 10*60*1000);
+    }
+
+    /**
+     * Fetching data.
+     */
+    fetchData () {
+        axios
+            .get(`https://openexchangerates.org/api/latest.json?app_id=7f2f509f57d84af8b386c403f163d9d4&&symbols=GBP,EUR,RUB`)
+            .then(res => {
+                const responseRates = res.data.rates;
+                let rates = [];
+
+                Object.keys(responseRates).map((rate) => {
+                    let rateItem = {};
+                    rateItem.currency = rate;
+                    rateItem.rate = responseRates[rate];
+                    rates.push(rateItem);
+                });
+
+                this.setState({ rates });
+            })
+            .catch((error) => console.log(error));
+    }
+
+    /**
+     * Render rates list.
+     */
+    renderRatesList () {
+        const { rates } = this.state;
+        let result = null;
+
+        if (rates.length > 1) {
+            result = (
+                <table className="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>Currency</th>
+                            <th>Rate</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            rates.map((rateItem, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td>{rateItem.currency}</td>
+                                        <td>{rateItem.rate}</td>
+                                    </tr>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table>
+            );
+        }
+
+        return result;
+    }
+    
+    render () {
+        return (
+            <div className="row">
+                <div className="col-xs-8 col-xs-offset-2">
+                    <div className="page-header">
+                        <h1>Currency rates widget <small>base currency - USD</small></h1>
+                    </div>
+
+                    {this.renderRatesList()}
+                </div>
+            </div>
+        );
+    }
+}
